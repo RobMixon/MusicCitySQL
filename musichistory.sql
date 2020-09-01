d05f9bdf1652519842d4a7dd020677951eb9a22b
Rob Mixon <mixonroberth@gmail.com>
2020-09-01 14:39:54 -0500

    code to review

--- musichistory.sql
+++ musichistory.sql
@@ -1,32 +1,75 @@
-﻿/*
-SELECT s.title,
-		a.ArtistName
-FROM Song s
-	LEFT JOIN Artist a on s.ArtistId = a.id
-;
-*/
+﻿select *
+from album;
 
-/*
-SELECT a.ArtistName
-FROM album al
-left JOIN Artist a on a.Id = al.ArtistId
-left JOIN Genre g ON g.Id = al.GenreId
-where (g.Label ='Jazz' OR g.Label = 'Rock');
-*/
-/*
-SELECT AlbumId, Count(AlbumId) as 'song count'
-from Song s
-GROUP BY AlbumId;
+SELECT ArtistID,
+       GenreID
+  FROM Album ;
 
-SELECT ArtistId, Count(ArtistId) as 'song count'
-from Song
-group by ArtistId;
-*/
 
-Select Title, AlbumLength
+  SELECT *
+  From Genre;
+
+
+
+SELECT DISTINCT 
+       a.ArtistName, GenreId
+  FROM Album s
+       LEFT JOIN Artist a on s.ArtistId = a.id
+       
+  where GenreId = 7;
+
+
+  
+SELECT DISTINCT 
+       a.ArtistName, GenreId
+  FROM Album s
+       LEFT JOIN Artist a on s.ArtistId = a.id
+       
+  where GenreId = 2 OR GenreID = 4;
+
+select Title
 FROM Album
-where AlbumLength = (SELECT Max(albumlength) from Album);
+where AlbumLength = 0;
 
-Select s.title, s.songlength, a.title
-from song s 
-left join album a on s.albumId = a.id where songlength = (select max(songlength) from song);
+/*INSERT INTO Artist (ArtistName, YearEstablished) Values ('Lindsey Stirling', 2010)*/
+
+/*INSERT INTO Album (Title, ReleaseDate, AlbumLength, Label, ArtistId, GenreId) VALUES ('Lindsey Stirling', '9/18/2012', 2711, 'Bridge Tone', 28, 14);*/
+/*INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES ('Crystallize', 258, '9/18/2012', 14, 28, 23);
+INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES ('Moon Trance', 185, '9/18/2012', 14, 28, 23);
+INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES ('Transcendence', 225, '9/18/2012', 14, 28, 23); */
+
+SELECT a.Title, s.Title, k.ArtistName 
+FROM Album a LEFT JOIN Song s ON s.AlbumId = a.Id
+LEFT JOIN Artist k ON s.ArtistId = k.id
+WHERE a.Title = 'By Lindsey Stirling';
+
+
+SELECT a.Title, COUNT(a.Title) as 'NumberofSongs'
+FROM Song s
+LEFT JOIN Album a ON s.AlbumID= a.Id
+GROUP BY a.Title;
+
+SELECT a.ArtistName, COUNT(Title)
+FROM Song s
+Left Join Artist a ON s.ArtistId = a.ID
+GROUP BY a.ArtistName
+
+SELECT g.Label, COUNT(Title)
+FROM Song s
+LEFT JOIN Genre g ON s.GenreId = g.Id
+GROUP BY g.Label;
+
+SELECT COUNT(DISTINCT a.Label) as 'Number of labels', ar.ArtistName
+FROM Album a
+LEFT JOIN Artist ar ON a.ArtistId = ar.id
+GROUP BY ar.ArtistName
+HAVING COUNT(DISTINCT a.Label) > 1;
+
+SELECT Title, Albumlength
+FROM Album
+WHERE AlbumLength = (SELECT MAX(AlbumLength) FROM Album)
+
+SELECT s.Title, s.SongLength, a.Title
+FROM Song s
+LEFT JOIN Album a ON s.AlbumId = a.Id
+WHERE SongLength = (SELECT MAX(SongLength) FROM SONG)songlength) from song);
